e7a0893fd54fbf6d95e1a8f9ac7c777b6ec658f8
Andreas Tarandi <torandi@gmail.com>
2012-03-04 11:59:10 +0100

    Trying to read test input

--- code/homeworkA/aes/main.c
+++ code/homeworkA/aes/main.c
@@ -4,6 +4,30 @@
 #include "aes.h"
 
 int main() {
+	char hex[32];
 	struct aes_t aes;
-	sprintf(aes.key, "Derp de herp");
+	fgets((char*)aes.key, 17, stdin);
+	int i;
+
+	/*for(i=0;i<4;++i) {
+		aes.key[i] = ntohl(aes.key[i]);
+	}*/
+	data_to_string(aes.key, hex);
+	printf("Key: %.32s\n", hex);
+	
+	while(!feof(stdin)) {	
+		fgets((char*)aes.iv, 17, stdin);
+		if(!feof(stdin)) {
+			for(i=0;i<4;++i) {
+			//	aes.iv[i] = ntohl(aes.iv[i]);
+			}
+			data_to_string(aes.iv, hex);
+			printf("Encryption block: %.32s\n", hex);
+			aes_encrypt(&aes);
+			data_to_string(aes.iv, hex);
+			printf("%.32s\n", hex);
+			fprintf(stderr, "%.32s\n", aes.iv);
+		}
+	} 
+	return 0;
 }
